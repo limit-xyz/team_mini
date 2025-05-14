b03e288747c995561c0fef646a3e8e8042b46bec
kimjonghyo <apekcjsrnr@Naver.com>
2025-05-14 13:40:24 +0900

    AdoptionDeleteService 추가

---
+++ src/main/java/com/miniproject/adoption/service/AdoptionDeleteService.java
@@ -0,0 +1,23 @@
+package com.miniproject.adoption.service;
+
+import java.io.IOException;
+
+import com.miniproject.dao.AdoptionDao01;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AdoptionDeleteService implements CommandProcess{
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int postId = Integer.parseInt((request.getParameter("postId"));
+		AdoptionDao01 dao = new AdoptionDao01();
+		dao.deleteAdoption(postId);
+		
+		return "redirec:/adoptionList.mvc";
+	}
+
+}
