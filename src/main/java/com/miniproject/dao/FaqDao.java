c9ffca974d6b70422e11778bb325383e5fa33505
orionrich <orionrich1@gmail.com>
2025-05-18 02:51:53 +0900

    faqDao 작업중 저장

--- src/main/java/com/miniproject/dao/FaqDao.java
+++ src/main/java/com/miniproject/dao/FaqDao.java
@@ -6,19 +6,91 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
 import com.miniproject.vo.Faq;
+
+
+// 전체 게시글 수를 읽어오는 메서드 - paging 처리에 사용
 
 
 public class FaqDao {
 	
-	private Connection conn;
-	private PreparedStatement pstmt;
-	private ResultSet rs;
 	
 	public FaqDao() {
 	}
 	
-	public ArrayList<Faq> getFaq() {
+	
+	//기본 db매니저 연결 
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	
+	
+	
+	
+	//CLUD U기능 FAQ의 수정폼에 불러올 내용 
+	//특정 FAQ 게시글중 하나의 모든 정보를 가져옴
+	public faq getFaqDetail(int faqNo) {
+		
+		String sqlFaq = SELECT faq_no, faq_title, faq_content, faq_author FROM FAQ WHERE faq_no = ?;
+				
+		return faq;
+	}
+	
+	// U기능 FAQ에 글을 수정하는 기능 
+	public int updateFaq(Faq faq) {
+		int result=0;
+		return result;
+	}
+	
+	// C기능 FAQ에 글 하나를 등록하는 기능 
+	// 관리자만 가능하게 처리함 
+	public int insertFaq(Faq faq) {
+		String sqlFaq = INSERT INTO FAQ (faq_no, faq_title, faq_content, faq_author) VALUES (SEQ_FAQ_NO.NEXTVAL, ?, ?, ?);
+		return result;
+	}
+	
+
+	
+	// D기능 FAQ에 글 하나를 지우는 기능 faqNo의 값으로 게시글 번호를 찾음
+	public int deleteFaq(int faqNo) {
+		String sqlFaq = DELETE FROM FAQ WHERE faq_no = ?; 
+		return result;
+	}
+	
+	
+	
+	
+	//faq의 수를 읽어오는 기능 페이징에 씀 
+	public int getFaqCount() {
+		String sqlFaq = "SELECT COUNT(*) FROM faq";
+		int count = 0;
+		
+		try {
+			conn = DBManager.getConnection();		
+			pstmt = conn.prepareStatement(sqlFaq);			
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}			
+		} catch(SQLException e) {
+			System.out.println("dao getFaqCount메서드 : SQLException");
+			e.printStackTrace();
+			
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		return count;		
+	}		
+	
+	
+	
+	
+	
+	//L기능 faqlist 전체를 받아오는 기능 
+	public ArrayList<Faq> getFaqList() {
 		
 		
 		String faqSql = "SELECT * FROM FAQ";
@@ -56,5 +128,10 @@ public class FaqDao {
 		
 		
 	}
+	
+	
+	
+
+
 
 }
